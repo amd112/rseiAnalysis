c8d11b44019a901c8b56e4f3df28ff9b392862d8
Anne Driscoll <amd112@duke.edu>
2017-11-12 11:19:50 -0500

    fixed NA handling for nonParametricExtrapolate

--- R/nonParametricExtrapolate.R
+++ R/nonParametricExtrapolate.R
@@ -9,8 +9,8 @@
 #' @examples nonParametricExtrapolate(t2010, t1990)
 nonParametricExtrapolate = function(year, old) {  
   #sampling white and black individuals with probability equal to their allocations in time = t
-  w_pop = sum(year$white)
-  n_pop = w_pop + sum(year$black)
+  w_pop = sum(year$white, na.rm = TRUE)
+  n_pop = w_pop + sum(year$black, na.rm = TRUE)
   sample = rbinom(n_pop/10000, 1, w_pop/n_pop) #so 1 represents white
   #drawing a concentration for each of the sampled individuals, now we have two collections of concentrations at t = 0
   w_samp = sample(old$lconcentration, sum(sample == 1), replace = TRUE, prob = old$white)
@@ -19,7 +19,8 @@ nonParametricExtrapolate = function(year, old) {
   density_0 = ewcdf(old$lconcentration, weights = old$tot_pop/sum(old$tot_pop))
   w_perc = density_0(w_samp)
   b_perc = density_0(b_samp)
-  #boxplot(w_perc, b_perc) #plotting the percentiles for each group
+  #boxplot(w_perc, b_perc) #
+  plotting the percentiles for each group
   #sampling the concentration value for the drawn percentile at time = t
   density_cur = ewcdf(year$lconcentration, weights = year$tot_pop/sum(year$tot_pop))
   w_est = quantile(density_cur, w_perc)
@@ -27,7 +28,6 @@ nonParametricExtrapolate = function(year, old) {
   w_tru = sample(year$lconcentration, sum(sample == 1), replace = TRUE, prob = year$white)
   b_tru = sample(year$lconcentration, sum(sample == 0), replace = TRUE, prob = year$black)
   #plotting the true time = t distribution, as well as the modeled time = t distribution
-  #ggplot() + stat_ecdf(aes(w_est), color = "red") + stat_ecdf(aes(b_est), color = "blue") + stat_ecdf(aes(w_tru), color = "orange") + stat_ecdf(aes(b_tru), color = "cornflowerblue") + xlim(-4, 14) 
-  #ggplot() + geom_density(aes(w_tru), color = "orange", fill = "orange", alpha = 0.2) + geom_density(aes(b_tru), color = "cornflowerblue", fill = "cornflowerblue", alpha = 0.2) + xlim(-2, 14) + geom_density(aes(w_est), color = "red") + geom_density(aes(b_est), color = "blue")
-  return(c(w_est, b_est, w_tru, b_tru))
+  ggplot() + geom_density(aes(w_tru), color = "orange", fill = "orange", alpha = 0.2) + geom_density(aes(b_tru), color = "cornflowerblue", fill = "cornflowerblue", alpha = 0.2) + xlim(-2, 14) + geom_density(aes(w_est), color = "red") + geom_density(aes(b_est), color = "blue")
+  return(list(w_est, b_est, w_tru, b_tru))
 }
